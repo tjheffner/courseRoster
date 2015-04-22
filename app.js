43ec40177f832bd84ed5d35f7168f0f99b0b31f6
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-22 10:33:55 -0700

    add second state / course controller + factory + partial

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var courseRoster = angular.module('courseRoster', ['ui.router']);
 
 courseRoster.config(function($stateProvider) {
   $stateProvider.state('home', {
-    url: "";
+    url: "",
     templateUrl: "partials/home.html"
   });
+
+  $stateProvider.state('courses', {
+    url: "/courses",
+    templateUrl: "partials/courses.html",
+    controller: 'CoursesCtrl'
+  });
+
 });
